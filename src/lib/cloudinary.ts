78a68b308ea7485facdd3db6b5f30bc7ede51cd7
Basit Saiyed <91619822+basitsaiyed@users.noreply.github.com>
2025-05-11 19:30:53 +0530

    Update cloudinary.ts

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -5,10 +5,10 @@ export const uploadImage = async (file: File) => {
   });
   const formData = new FormData();
   formData.append('file', file);
-  formData.append('upload_preset', process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!);
+  formData.append('upload_preset', import.meta.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!);
 
   const response = await fetch(
-    `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+    `https://api.cloudinary.com/v1_1/${import.meta.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
     {
       method: 'POST',
       body: formData,
