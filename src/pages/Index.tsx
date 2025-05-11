4a36016db1d60631b34dfef9d21356180ae89eb8
Basit Saiyed <91619822+basitsaiyed@users.noreply.github.com>
2025-05-11 22:51:42 +0530

    Update Toastmaster International photo

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -92,7 +92,7 @@ const Index = () => {
             <div className="md:w-1/2 w-full">
               <div className="relative aspect-[4/3] md:aspect-[16/9]">
                 <img
-                  src="https://www.pngwing.com/en/search?q=toastmasters+International"
+                  src="https://res.cloudinary.com/dtn8ucfqp/image/upload/v1746984053/upscalemedia-transformed_wj7d4b.png"
                   alt="Toastmasters International"
                   className="rounded-lg shadow-xl w-full h-full object-cover"
                 />
