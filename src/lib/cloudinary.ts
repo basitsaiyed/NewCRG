79452b97e92c2c0d81c7d26c74c6e76050bc24f4
Basit Saiyed <91619822+basitsaiyed@users.noreply.github.com>
2025-05-11 19:34:34 +0530

    Update cloudinary.ts

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,11 +1,11 @@
 export const uploadImage = async (file: File) => {
   console.log('Cloudinary Config:', {
-    cloudName: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-    uploadPreset: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET
+    cloudName: import.meta.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    uploadPreset: import.meta.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET
   });
   const formData = new FormData();
   formData.append('file', file);
-  formData.append('upload_preset', import.meta.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!);
+  formData.append('upload_preset', import.meta.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET);
 
   const response = await fetch(
     `https://api.cloudinary.com/v1_1/${import.meta.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
